326064864ae62efd92b40393e23ffd6ed75ede8e
MateusFRz <minecraftantoni@gmail.com>
2019-11-22 03:35:21 +0100

    create Connection class and view body, header

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 
+/*
+ * error_reporting(-1);
+ *ini_set('display_errors', 'On');
+*/
+
+require_once ("view/vue.php");
--- view/header.php
+++ view/header.php
@@ -3,4 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Site de gestions de tâches</title>
+
+    <link rel="stylesheet" href="view/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="view/css/bootstrap-grid.min.css"/>
+    <link rel="stylesheet" href="view/css/bootstrap-reboot.min.css"/>
+
+    <script async src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+    <script async src="view/js/bootstrap.min.js"></script>
+    <script async src="view/js/bootstrap.bundle.min.js"></script>
 </head>
