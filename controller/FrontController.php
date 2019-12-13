b09750d9d71a41ea6d88498d2a2c28482fecea06
andilella <antoni.di_lella@etu.uca.fr>
2019-12-13 20:48:14 +0100

    update FrontController.php

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -7,43 +7,79 @@ class FrontController {
      */
     public function __construct() {
         global $errors, $rep;
-        $action = NULL;
+        $action = $_REQUEST['action'];
 
         session_start();
 
-        if (isset($_REQUEST['action']))
-            $action = $_REQUEST['action'];
+        $actions = array(
+            'visitor' => [NULL, 'login', 'signup', 'signupPage', 'loginPage', 'publicPage', 'removeTask', 'addChecklist', 'changeTaskState', 'removeChecklist', 'modifyChecklist', 'addTask', 'updateTask'],
+            'user' => ['profile', 'private', 'logout']
+        );
 
+        Validation::purify($action);
         try {
-            switch ($action) {
-                case "publicPage":
-                case NULL:
-                    new VisitorController();
-                    break;
-                case "profile":
-                case "private":
-                case "logout":
-                case "login":
-                case "removeTask":
-                case "addChecklist":
-                case "changeTaskState":
-                case "removeChecklist":
-                case "modifyChecklist":
-                case "addTask":
-                case "updateTask":
-                case "signup":
-                case "signupPage" :
-                case "loginPage":
-                    new FrontUserController($action);
-                    break;
-                default:
-                    throw new Exception('Action no found !', 404);
-            }
+            $role = array_search($action, $actions);
+            if ($role === "user") {
+                if (Validation::isUser($_SESSION['user'])) {
+                    switch ($action) {
+                        case "profile":
+                            UserController::profile();
+                            return;
+                        case "private":
+                            UserController::privateChecklist();
+                            break;
+                        case "logout":
+                            UserController::logout();
+                            break;
+                    }
+                } else
+                    throw new Exception('You need to be connected');
+            } else if ($role === "visitor") {
+                switch ($action) {
+                    case NULL:
+                    case "publicPage":
+                        VisitorController::publicPage();
+                        return;
+                    case "removeTask":
+                        TaskController::removeTask();
+                        break;
+                    case "addChecklist":
+                        TaskController::addTask();
+                        break;
+                    case "changeTaskState":
+                        TaskController::changeTaskState();
+                        break;
+                    case "updateTask":
+                        TaskController::updateTask();
+                        break;
+                    case "removeChecklist":
+                        ChecklistController::removeChecklist();
+                        break;
+                    case "modifyChecklist":
+                        ChecklistController::modifyChecklist();
+                        break;
+                    case "addTask":
+                        ChecklistController::addChecklist();
+                        break;
+                    case "login":
+                        UserController::login();
+                        return;
+                    case "signup":
+                        UserController::signup();
+                        return;
+                    case "signupPage" :
+                        UserController::signupPage();
+                        return;
+                    case "loginPage":
+                        UserController::loginPage();
+                        return;
+                }
+            } else
+                throw new Exception('You try something not valid !', 400);
         } catch (Exception $exception) {
             $errors['exception'] = $exception->getMessage();
         }
 
-        if (!empty($errors))
-            require_once $rep . "/view/vue.php";
+        require_once $rep . "/view/vue.php";
     }
 }
